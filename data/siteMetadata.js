c99d69f0ce01d5ae61213cf6d89c0de53014c830
Tomasz Smolarczyk <smolarczyk.thomas@gmail.com>
2024-01-07 13:50:14 +0100

    info site changed

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,22 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'tsmolarczyk',
+  author: 'Tom Smolarczyk',
+  headerTitle: 'tsmolarczyk',
+  description: 'Journaling my journey as a developer',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://tsmolarczyk.vercel.app',
+  siteRepo: 'https://github.com/tsmolarczyk/tsmolarczyk-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
+  email: 'tomek.smolarczyk@gmail.com',
+  github: 'https://github.com/tsmolarczyk',
   twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/tsmolarczyk/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
